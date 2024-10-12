cc66403dcf7835b60696a14bbf603f16b36d6f31
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-10-12 23:38:24 +0330

    build database model and register model paneladmin

--- ecom/store/admin.py
+++ ecom/store/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Category, Customer, Product, Order
+
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+# Registering the Customer model
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'email')
+    search_fields = ('first_name', 'last_name', 'email', 'phone')
+    list_filter = ('email',)
+    ordering = ('first_name', 'last_name')
+
+
+# Registering the Product model
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category', 'description')
+    search_fields = ('name', 'category__name')
+    list_filter = ('category',)
+    ordering = ('name', 'category')
+
+
+# Registering the Order model
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'product', 'quantity', 'status', 'date_ordered', 'phone', 'address')
+    search_fields = ('customer__first_name', 'customer__last_name', 'product__name', 'phone')
+    list_filter = ('status', 'date_ordered')
+    ordering = ('date_ordered', 'status')
--- ecom/store/models.py
+++ ecom/store/models.py
@@ -1,3 +1,58 @@
 from django.db import models
-
+import datetime
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.hashers import make_password
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = PhoneNumberField(region="US")
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def save(self, *args, **kwargs):
+        if self.password:
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,default=1)
+    description = models.TextField(max_length=500, blank=True, null=True)
+    image = models.ImageField(upload_to='products/')
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=500)
+    phone = PhoneNumberField(region="US")
+    date_ordered = models.DateTimeField(default=datetime.datetime.now)
+    status = models.BooleanField(default=False)
+
+
+
+
+
+
