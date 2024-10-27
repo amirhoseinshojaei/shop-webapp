b7960ea02f2c379aed98f3b22e1df739401fb0b7
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-10-27 23:58:32 +0330

    build add function

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -16,3 +16,13 @@ class Cart():
         # Make sure cart is available all pages
         self.cart = cart
 
+    def add(self, product):
+        product_id = str(product.id)
+
+        # Logic
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+
+        self.session.modified = True   
